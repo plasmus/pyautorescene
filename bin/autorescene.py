c988e28e30c9a466e2809dd362b62fe623e76c1a
jaloj <81585183+jaloj@users.noreply.github.com>
2021-11-12 22:01:35 +0000

    Added feature to only add sample/proof if release already rescened

--- bin/autorescene.py
+++ bin/autorescene.py
@@ -89,13 +89,16 @@ def search_by_crc(crc):
 
     return data['results']
 
-def search_by_name(name):
+def search_by_name(name, isdir = False):
     if not name or name == "":
         raise ValueError("Release must have a valid name")
 
-    name_search = srrdb_api + "r:" + name.rsplit( ".", 1 )[ 0 ]
-		
-    try:		
+    if not isdir:
+        name_search = srrdb_api + "r:" + name.rsplit( ".", 1 )[ 0 ]
+    else:
+        name_search = srrdb_api + "r:" + name
+
+    try:
         response = s.retrieveContent(name_search)
         data = response.json()
     except:
@@ -111,7 +114,7 @@ def search_by_oso(hashfile):
         raise ValueError("Release must have a valid OSO hash")
 
     name_search = srrdb_api + "isdbhash:" + hashfile
-		
+
     try:
         response = s.retrieveContent(name_search)
         data = response.json()
@@ -235,7 +238,7 @@ def search_srrdb_crc(crc, rlspath):
         verbose("\t - Searching srrdb.com for matching release name", end="")
         try:
             rlsname = os.path.basename(rlspath)
-            results = search_by_name(rlsname)
+            results = search_by_name(rlsname, isdir = False)
         except Exception as e:
             verbose("%s -> %s" % (FAIL, e))
             return False
@@ -267,6 +270,26 @@ def search_srrdb_crc(crc, rlspath):
 
     return release
 
+def search_srrdb_dirname(rlspath):
+    #search srrdb for release matching
+    verbose("\t - Searching srrdb.com for matching release name", end="")
+    try:
+        rlsname = os.path.basename(rlspath)
+        results = search_by_name(rlsname, isdir = True)
+    except Exception as e:
+        verbose("%s -> %s" % (FAIL, e))
+        return False
+
+    if not results or len(results) > 1:
+        verbose("%s -> %s" % (FAIL, "No matching results"))
+        return False
+    else:
+        verbose("%s" % SUCCESS)
+
+    release = results[0]
+    verbose("\t\t - Matched release: %s" % release['release'])
+
+    return release
 def check_file(args, fpath):
     if not os.path.splitext(fpath)[1] in args['extension']:
         return False
@@ -428,7 +451,142 @@ def check_file(args, fpath):
                     release_list[release['release']]['resample'] = True
             else:
                 missing_files.append(release['release']+"/Sample/"+sample.get_filename())
+        else:
+            verbose("\t - No SRS found for sample recreation %s" % (FAIL))
 
+        if args['find_subs']:
+            print("finding subs")
+
+def check_dir(args, fpath):
+    if os.path.basename(fpath.lower()) == "Proof".lower():
+        return False
+    if os.path.basename(fpath.lower()) == "Sample".lower():
+        return False
+    if os.path.basename(fpath.lower()) == "Sub".lower():
+        return False
+    if os.path.basename(fpath.lower()) == "Subs".lower():
+        return False
+
+    if args['output']:
+        doutput = args['output']
+    else:
+        doutput = os.path.dirname(fpath)
+
+    verbose("* Found potential release: " + os.path.basename(fpath))
+
+    release = search_srrdb_dirname(fpath)
+    if not release:
+        return False
+    else:
+        #keep track of the releases we are processing
+        if not release['release'] in release_list:
+            release_list[release['release']] = dict()
+            release_list[release['release']]['rescene'] = True
+            release_list[release['release']]['resample'] = False
+            release_list[release['release']]['extract'] = False
+        elif release_list[release['release']]['rescene'] and release_list[release['release']]['resample']:
+            verbose("\t - Skipping, already processed.")
+            return True
+
+    verbose("\t - Downloading SRR from srrdb.com", end="")
+    # download srr
+    try:
+        srr_path = download_srr(release['release'])
+    except Exception as e:
+        verbose("%s -> %s" % (FAIL, e))
+        return False
+    else:
+        verbose("%s" % SUCCESS)
+
+    release_srr = SRR(srr_path)
+#    srr_finfo = release_srr.get_archived_fname_by_crc(release_crc)
+
+    if os.path.basename(doutput.lower()).lower() != release['release'].lower():
+        #output dir is not specific to rls/doesnt match release
+        doutput = os.path.join(doutput, release['release'])
+        if not os.path.isdir(doutput):
+            verbose("\t - Creating output directory: %s" % (doutput), end="")
+            try:
+                mkdir(doutput)
+            except Exception as e:
+                verbose("%s -> Unable to create directory: %s" % (FAIL, e))
+                return False
+            else:
+                verbose("%s" % (SUCCESS))
+
+    verbose("\t - Setting output directory to: %s" % doutput)
+
+    if (args['extract_stored'] or args['auto_reconstruct']) and not release_list[release['release']]['extract']:
+        verbose("\t - Extracting stored files from SRR", end="")
+        for fname in os.listdir(fpath):
+            if fname.endswith(".rar"):
+                rar_path = os.path.join(fpath, fname)
+        try:
+            matches = release_srr.extract_stored_files_regex(doutput)
+
+        except Exception as e:
+            verbose("%s -> %s" % (FAIL, e))
+            return False
+
+        else:
+            verbose("%s" % SUCCESS)
+
+            for match in matches:
+                if match[0].endswith(".srs"):
+                    srs_path = match[0]
+
+                verbose("\t\t - %s" % os.path.relpath(match[0], doutput))
+            release_list[release['release']]['extract'] = True
+
+    if (args['resample'] or args['auto_reconstruct']) and not release_list[release['release']]['resample']:
+        if release['hasSRS'] == "yes":
+            try:
+                srs_path
+            except NameError:
+                srs_path = ""
+
+                verbose("\t\t - Extracting SRS from SRR file for Sample reconstruction", end="")
+                release_srs = release_srr.get_srs(doutput)
+
+                if len(release_srs) != 1:
+                    verbose("%s -> more than one SRS in this SRR.  Please reconstruct manually." % FAIL)
+                else:
+                    srs_path = release_srs[0][0]
+
+            if srs_path != "":
+                sample = SRS(srs_path)
+                verbose("\t - Recreating Sample .. expect output from SRS\n-------------------------------")
+
+                try:
+                    sample.recreate(rar_path, os.path.dirname(srs_path))
+                except Exception as e:
+                    verbose("-------------------------------")
+                    verbose("\t - %s -> failed to recreate sample: %s." % (FAIL,e))
+
+                    #sample reconstruction failed.. should we try check local disk for a sample?
+                    if args['find_sample']:
+                        verbose("\t - Searching for sample on local disk")
+                        sample_file = find_file(os.path.dirname(fpath), sample.get_filename(), sample.get_crc())
+                        if sample_file:
+                            verbose("\t\t - %s - Found sample -> %s" % (SUCCESS, sample_file))
+                            try:
+                                print(sample_file)
+                                print(os.path.dirname(srs_path))
+                                copy_file(sample_file, os.path.dirname(srs_path))
+                                release_list[release['release']]['resample'] = True
+                            except Exception as e:
+                                verbose("\t\t - %s - Could not copy file to %s -> %s" % (FAIL, os.path.dirname(srs_path), e))
+                                missing_files.append(release['release']+"/Sample/"+sample.get_filename())
+                        else:
+                            missing_files.append(release['release']+"/Sample/"+sample.get_filename())
+                    else:
+                        missing_files.append(release['release']+"/Sample/"+sample.get_filename())
+                else:
+                    verbose("-------------------------------")
+                    verbose("\t - %s -> sample recreated successfully" % (SUCCESS))
+                    release_list[release['release']]['resample'] = True
+            else:
+                missing_files.append(release['release']+"/Sample/"+sample.get_filename())
         else:
             verbose("\t - No SRS found for sample recreation %s" % (FAIL))
 
@@ -472,6 +630,8 @@ if __name__ == "__main__":
             check_file(args, path)
         elif os.path.isdir(path):
             for root, dirs, files in os.walk(path):
+                for dirname in dirs:
+                        check_dir(args, os.path.join(root, dirname))
                 for sfile in files:
                     check_file(args, os.path.join(root, sfile))
 
